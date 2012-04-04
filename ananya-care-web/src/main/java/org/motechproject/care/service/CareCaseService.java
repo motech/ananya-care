f4f59b6469cfcf28d4bb536c77277fd51f0751fc
pchandra <pchandra@thoughtworks.com>
2012-04-04 10:41:52 +0530

    Praveen, Sharvani | #000| Added the writer according to motech-contrib implementation

--- ananya-care-web/src/main/java/org/motechproject/care/service/CareCaseService.java
+++ ananya-care-web/src/main/java/org/motechproject/care/service/CareCaseService.java
@@ -6,8 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//@Component
-//@Path("/case")
+import java.io.IOException;
+import java.io.Writer;
+
 @Controller
 @RequestMapping("/case/**")
 public class CareCaseService extends CaseService<CareCase> {
@@ -17,23 +18,29 @@ public class CareCaseService extends CaseService<CareCase> {
     }
    
     @Override
-    public void closeCase(CareCase ccCase) {
+    public void closeCase(CareCase ccCase,Writer writer) {
         String household_id = ccCase.getHousehold_id();
     }
 
     @Override
-    public void updateCase(CareCase ccCase) {
+    public void updateCase(CareCase ccCase,Writer writer) {
     }
 
     @Override
-    public void createCase(CareCase ccCase) {
+    public void createCase(CareCase ccCase,Writer writer) {
+        try {
+            writer.write("success");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Case received: " +ccCase.getCase_name());
         System.out.println("primary contact name: " +ccCase.getPrimary_contact_name());
     }
 
-   // @GET
     @RequestMapping(value="/test",method = RequestMethod.GET)
     public String test(){
         return  "Hello World";
     }
+
 }
