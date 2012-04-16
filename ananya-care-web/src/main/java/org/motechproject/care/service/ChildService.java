72e83a1d519717668e7be0a4f85c4431057c15be
Vandana <vandanag@vandanag-Latitude-E6510.(none)>
2012-04-16 17:42:53 +0530

    Sharvani,Vandana|#9 - Added logic to check age of the child before enrolling him

--- ananya-care-web/src/main/java/org/motechproject/care/service/ChildService.java
+++ ananya-care-web/src/main/java/org/motechproject/care/service/ChildService.java
@@ -1,11 +1,13 @@
 package org.motechproject.care.service;
 
+import org.joda.time.DateTime;
 import org.motechproject.care.domain.Child;
 import org.motechproject.care.repository.AllChildren;
 import org.motechproject.care.repository.AllMothers;
 import org.motechproject.care.request.CareCase;
 import org.motechproject.care.schedule.service.CareScheduleTrackingService;
 import org.motechproject.care.service.mapper.ChildMapper;
+import org.motechproject.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,13 +33,21 @@ public class ChildService {
 
     private void createUpdate(Child child) {
         if (doesMotherNotExist(child)) return;
+        DateTime childDOB = allMothers.findByCaseId(child.getMotherCaseId()).getAdd();
+        child.setDOB(childDOB);
         Child childFromDb = allChildren.findByCaseId(child.getCaseId());
         if(childFromDb ==null){
-           allChildren.add(child);
+            if(isOlderThanAYear(child))
+                return;
+            allChildren.add(child);
             return;
         }
         childFromDb.setValuesFrom(child);
         allChildren.update(childFromDb);
+    }
+
+    private boolean isOlderThanAYear(Child child) {
+        return !DateUtil.today().minusYears(1).isBefore(child.getDOB().toLocalDate());
     }
 
     private boolean doesMotherNotExist(Child child) {
--- ananya-care-web/src/test/java/org/motechproject/care/service/ChildServiceTest.java
+++ ananya-care-web/src/test/java/org/motechproject/care/service/ChildServiceTest.java
@@ -40,11 +40,13 @@ public class ChildServiceTest {
 
 
     @Test
-    public void shouldSaveChildIfDoesNotExist_WhenMotherExists() {
+    public void shouldSaveChildIfDoesNotExist_WhenMotherExistsAndAgeLessThanAYear() {
         String caseId = "caseId";
         CareCase careCase = new ChildCareCaseBuilder().withCaseId(caseId).withBabyMeaslesDate("2012-02-01").withVitamin1Date("2012-08-07").withCaseType(CaseType.Child.getType()).build();
         when(allChildren.findByCaseId(caseId)).thenReturn(null);
-        when(allMothers.findByCaseId(careCase.getMother_id())).thenReturn(new Mother());
+        Mother mother = new Mother();
+        mother.setAdd(DateTime.now().minusMonths(1));
+        when(allMothers.findByCaseId(careCase.getMother_id())).thenReturn(mother);
         ArgumentCaptor<Child> captor = ArgumentCaptor.forClass(Child.class);
         childService.process(careCase);
         verify(allChildren).add(captor.capture());
@@ -67,4 +69,43 @@ public class ChildServiceTest {
         verify(allChildren,never()).add((Child) Matchers.any());
     }
 
+    @Test
+    public void shouldNotSaveChildIfAgeMoreThanAYear() {
+        String caseId = "caseId";
+        String motherId = "motherId";
+        DateTime motherAdd = new DateTime(2011, 4, 13, 0, 0);
+        Mother mother = new Mother(motherId);
+        mother.setAdd(motherAdd);
+        CareCase careCase = new ChildCareCaseBuilder().withCaseId(caseId).withCaseType(CaseType.Child.getType()).withMotherCaseId(motherId).build();
+        when(allChildren.findByCaseId(caseId)).thenReturn(null);
+        when(allMothers.findByCaseId(motherId)).thenReturn(mother);
+        childService.process(careCase);
+        verify(allChildren,never()).add((Child) Matchers.any());
+    }
+
+    @Test
+    public void shouldUpdateChildIfAgeMoreThanAYear() {
+        String caseId = "caseId";
+        String motherId = "motherId";
+        String oldName = "Aryan";
+        String newBcgDate = "2012-05-04";
+        String newName = "Vijay";
+        DateTime motherAdd = new DateTime(2011, 4, 12, 0, 0);
+        Mother mother = new Mother(motherId);
+        mother.setAdd(motherAdd);
+        CareCase careCase = new ChildCareCaseBuilder().withCaseId(caseId).withCaseType(CaseType.Child.getType()).withMotherCaseId(motherId).withCaseName(newName).withBcgDate(newBcgDate).build();
+        Child childInDb = new Child(caseId);
+        childInDb.setName(oldName);
+        ArgumentCaptor<Child> captor = ArgumentCaptor.forClass(Child.class);
+
+        when(allChildren.findByCaseId(caseId)).thenReturn(childInDb);
+        when(allMothers.findByCaseId(motherId)).thenReturn(mother);
+        childService.process(careCase);
+
+        verify(allChildren,never()).add((Child) Matchers.any());
+        verify(allChildren).update(captor.capture());
+        Child child = captor.getValue();
+        Assert.assertEquals(newName,child.getName());
+        Assert.assertEquals(DateTime.parse(newBcgDate),child.getBcgDate());
+    }
 }
