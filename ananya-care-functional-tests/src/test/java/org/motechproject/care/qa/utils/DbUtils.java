a1bc55d19657a8726b0fd944d2037abaabb13a09
Ashish Kumar Mishra <ashishkm@thoughtworks.com>
2012-05-04 13:35:12 +0530

    Vandana, Ashish | Fixing failing tests

--- ananya-care-functional-tests/src/test/java/org/motechproject/care/qa/utils/DbUtils.java
+++ ananya-care-functional-tests/src/test/java/org/motechproject/care/qa/utils/DbUtils.java
@@ -7,11 +7,9 @@ import org.motechproject.care.repository.AllMothers;
 import org.motechproject.commcarehq.domain.AlertDocCase;
 import org.motechproject.commcarehq.repository.AllAlertDocCases;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 public class DbUtils {
 
     private AllChildren allChildren;
