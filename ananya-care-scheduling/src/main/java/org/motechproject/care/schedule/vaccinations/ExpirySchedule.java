53d8a9fe490c347d56c4b728793c98e06f421fb4
rahul0996 <rgarg@thoughtworks.com>
2012-05-23 18:26:39 +0530

    Rahul | #00 | removing static methods

--- ananya-care-scheduling/src/main/java/org/motechproject/care/schedule/vaccinations/ExpirySchedule.java
+++ ananya-care-scheduling/src/main/java/org/motechproject/care/schedule/vaccinations/ExpirySchedule.java
@@ -1,7 +1,5 @@
 package org.motechproject.care.schedule.vaccinations;
 
-import java.util.ArrayList;
-
 public enum ExpirySchedule {
     ChildCare("Child Care"),
     MotherCare("Mother Care");
@@ -14,13 +12,5 @@ public enum ExpirySchedule {
 
     public String getName() {
         return vaccinationScheduleName;
-    }
-
-    public static ArrayList<String> allVaccineNames() {
-        ArrayList<String> vaccines = new ArrayList<String>();
-        for (ExpirySchedule b : ExpirySchedule.values()) {
-            vaccines.add(b.getName());
-        }
-        return vaccines;
     }
 }
