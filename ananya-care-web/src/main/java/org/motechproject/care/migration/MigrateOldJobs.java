cc6e174b6f1fc3e9c7e659c6f54b5a325121b763
Shreyas Navada <shreyasnavada.91@gmail.com>
2014-05-01 12:03:34 +0530

    Shreyas | log4j to log the migration logs in a seperate file

--- ananya-care-web/src/main/java/org/motechproject/care/migration/MigrateOldJobs.java
+++ ananya-care-web/src/main/java/org/motechproject/care/migration/MigrateOldJobs.java
@@ -7,9 +7,13 @@ import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
+import org.motechproject.care.schedule.vaccinations.ExpirySchedule;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.scheduler.MotechSchedulerService;
 import org.motechproject.scheduler.domain.RunOnceSchedulableJob;
+import org.motechproject.scheduletracking.api.domain.WindowName;
+import org.motechproject.scheduletracking.api.events.MilestoneEvent;
+import org.motechproject.scheduletracking.api.events.constants.EventSubjects;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.JobKey;
@@ -44,15 +48,33 @@ public class MigrateOldJobs {
     private static final int HOUR_INTERVAL = 1;                
     /* Time interval Between each trigger is updated for each trigger */
     private static int intervalBetweenJobs = 0 ;
+    
+    private List<String> expiryCases = new ArrayList();
+    
+    private List<String> expiryMilestones = new ArrayList();
+    
+    public void populateExpiryCases(){
+    	expiryCases.add(ExpirySchedule.ChildCare.getName());
+    	expiryCases.add(ExpirySchedule.MotherCare.getName());
+    }
+    
+    public void populateExpiryMilestones(){
+    	expiryMilestones.add("OPV 0");
+    	expiryMilestones.add("Bcg");
+    	expiryMilestones.add("Hep 0");
+    }
+    
 
-    public MigrateOldJobs(MotechSchedulerService motechSchedulerService){
+    public MigrateOldJobs(MotechSchedulerService motechSchedulerService ){
         this.motechSchedulerService = motechSchedulerService;
+        populateExpiryCases();
+        populateExpiryMilestones();
         StdSchedulerFactory schedulerFactoryBean;
         try {
 	    schedulerFactoryBean = new StdSchedulerFactory("quartzForMigration.properties");
 	    scheduler = schedulerFactoryBean.getScheduler();
         } catch (SchedulerException e) {
-	e.printStackTrace();
+        	e.printStackTrace();
         }
     }
 
@@ -64,11 +86,25 @@ public class MigrateOldJobs {
 	scheduler.standby();
     }
 
-    public void rescheduleJobs(Trigger trigger, JobDetail jobDetail){
+    public boolean rescheduleJobs(Trigger trigger, JobDetail jobDetail){
         MotechEvent motechEvent = getEventDataFromJob(jobDetail);
+        boolean rescheduleJob = false;
+        MilestoneEvent event = new MilestoneEvent(motechEvent);
+        if(motechEvent.getSubject().equals(EventSubjects.MILESTONE_ALERT) ) {
+        	String currentMilestone = event.getMilestoneAlert().getMilestoneName() ;
+        	if(expiryCases.contains(currentMilestone) || (expiryMilestones.contains(currentMilestone) && event.getWindowName().equals(WindowName.late.name()) )){
+        	rescheduleJob = true;
+        	}// Reschedule only for child care and Mother Care as they send the close cases
+    	}
+        if(rescheduleJob){
         Date startTime = getStartDateForTrigger(trigger);
+        if(startTime == null){
+            startTime = trigger.getStartTime();
+        }
         RunOnceSchedulableJob runOnceSchedulableJob = new RunOnceSchedulableJob(motechEvent,startTime);
         motechSchedulerService.safeScheduleRunOnceJob(runOnceSchedulableJob);
+        }
+        return rescheduleJob;
     }
 
     public MotechEvent getEventDataFromJob(JobDetail jobDetail){
@@ -81,16 +117,18 @@ public class MigrateOldJobs {
         return new MotechEvent(eventType, params);
     }
 
-    /**
-     * Start time of the job remains same as the old jobs if the job is scheduled in sometime future.
-      If the job is supposed to run in past, Reschedule the job to run after particular time(@HOUR_INTERVAL)*/
+
+    /* Start time of the job remains same as the old jobs if the job is scheduled in sometime future.
+    If the job is supposed to run in past, Reschedule the job to run after particular time(@HOUR_INTERVAL)*/
+
     public Date getStartDateForTrigger(Trigger trigger){
         Date startDate = trigger.getStartTime();
         if(startDate.before(new Date())){
 	    DateTime startTime = new DateTime().plusHours(HOUR_INTERVAL);
 	    startDate = startTime.plusSeconds(intervalBetweenJobs++*3).toDate();  //  Interval between two jobs is one second for misfired jobs
-        }
         return startDate;
+        }
+        return null;
     }
 
     /*JobKey Name will be in format : jobType-jobId */
@@ -103,19 +141,22 @@ public class MigrateOldJobs {
     public JobDetail getJobDetail(Trigger trigger) throws SchedulerException{
 	 return scheduler.getJobDetail(trigger.getJobKey());
     }
-	
+
     public void runMigration() throws SchedulerException{
         logger.info("Starting Scheduler");
         setSchedulertoStandBy();							                 //   To stop the misfire of the old jobs.
         logger.info("Scheduler Standby");
         List<TriggerKey> triggers = getOldJobs();
         Trigger trigger = null ;
-    	for(TriggerKey triggerKey : triggers)
+        for(TriggerKey triggerKey : triggers)
     	{
 	    logger.info("trigger for Trigger Key :" + triggerKey);
 	    trigger = scheduler.getTrigger(triggerKey);
-	    rescheduleJobs(trigger, getJobDetail(trigger));
-	    scheduler.deleteJob(trigger.getJobKey());
-        }
+	    if(rescheduleJobs(trigger, getJobDetail(trigger)))
+	    	scheduler.deleteJob(trigger.getJobKey());
+    	}
     }
+
+    
+
 }
